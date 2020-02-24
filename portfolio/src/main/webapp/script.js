5bc85bb1935138c81b7926f6de79aab68ca5fdc2
SamuelLellis <slellis1@binghamton.edu>
2020-02-23 22:47:55 -0500

    Update button and text

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,13 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['OW!', 'Ouch', "Please stop", "Hey!"];
+      ["Never gonna give you up",
+"Never gonna let you down",
+"Never gonna run around and desert you",
+"Never gonna make you cry",
+"Never gonna say goodbye",
+"Never gonna tell a lie and hurt you",
+"HAHAHA Rick ROll'd. Be more careful next time ;)"];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
